18abfc0b67085b5de98ed95a30801c375cf9da85
deepthishivani <deepthishivani@gmail.com>
2026-02-24 10:43:15 +0530

    UC5: Array initialization using String.join()

---
+++ src/use_case/use_case5.java
@@ -0,0 +1,15 @@
+package use_case;
+public class use_case5 {
+    public static void main(String[] args) {
+        String[] banner = {
+                String.join("", "********************************"),
+                String.join("", "*                              *"),
+                String.join("", "*            OOPS              *"),
+                String.join("", "*                              *"),
+                String.join("", "********************************")
+        };
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
