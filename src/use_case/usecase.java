61809713235235d85d5ce2f9c0e4251ba93543f3
deepthishivani <deepthishivani@gmail.com>
2026-03-10 18:06:47 +0530

    UC6: Introduced methods for character patterns

--- src/use_case/usecase.java
+++ src/use_case/usecase.java
@@ -2,25 +2,44 @@ package use_case;
 
 public class usecase {
 
-    static String[] banner = {
-        "*******   *******   *****   ******",
-        "*     *   *     *   *   *   *     *",
-        "*     *   *     *   *   *   *****",
-        "*     *   *     *   *   *   *",
-        "*******   *******   *****   ******"
-    };
-
     public static void main(String[] args) {
 
-        printBanner();
+        String[] O = patternO();
+        String[] P = patternP();
+        String[] S = patternS();
 
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "   " + O[i] + "   " + P[i] + "   " + S[i]);
+        }
     }
 
-    static void printBanner() {
+    static String[] patternO() {
+        return new String[]{
+                "*******",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*******"
+        };
+    }
 
-        for (String line : banner) {
-            System.out.println(line);
-        }
+    static String[] patternP() {
+        return new String[]{
+                "*****",
+                "*   *",
+                "*****",
+                "*",
+                "*"
+        };
+    }
 
+    static String[] patternS() {
+        return new String[]{
+                "******",
+                "*     *",
+                "*****",
+                "*",
+                "******"
+        };
     }
 }
